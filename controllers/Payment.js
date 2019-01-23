2d13a3afef75fd785d4627b5927867d7239d5ba2
Everistus Olumese <everistusolumese@gmail.com>
2019-01-23 06:44:49 +0100

    Sure

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -54,7 +54,7 @@ module.exports = {
                 
                
             }).catch(err =>{
-                res.status(500).json({success: false, message: 'Payment not successful'});
+                res.status(500).json({success: false, message: 'Payment not successful', err});
             })
         }else{
             res.status(401).json({success: false, message: 'You must sign in before making a purchase'});
