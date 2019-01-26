86aa9195f69ef8a0dc19c5ed706c358c51e90810
Everistus Olumese <everistusolumese@gmail.com>
2019-01-26 06:44:43 +0100

    Adding otp

--- routes/farm.js
+++ routes/farm.js
@@ -8,7 +8,7 @@ const FarmController = require('../controllers/Farms')
 router.get('/v1/', FarmController.getAllFarms)
 router.post('/v1/', verifyToken, FarmController.createFarm)
 router.patch('/v1/:id', verifyToken, FarmController.editFarm)
-router.post('/v1/widthdraw', verifyToken, FarmController.withDrawROI)
+router.post('/v1/withdraw', verifyToken, FarmController.withDrawROI)
 
 
 module.exports = router
