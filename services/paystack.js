576f8463df0f605d452952d28ada784c8d8c1cbe
Everistus Olumese <everistusolumese@gmail.com>
2019-01-30 10:06:52 +0100

    Fixing authentication error messages

--- services/paystack.js
+++ services/paystack.js
@@ -54,9 +54,9 @@ module.exports = {
         return new Promise((resolve, reject) => {
 
             axios.interceptors.response.use((response) => {
-                fs.writeFileSync( path.join(__dirname, '..', 'success.log'), JSON.stringify(response.data))
-                winston.info("Line 54 paystack intec", response.data.data)
-                return response.data.data;
+                fs.writeFileSync( path.join(__dirname, '..', 'success.log'), JSON.stringify(response))
+                winston.info("Line 54 paystack intec", response)
+                return response;
             }, function (error) {
                 
                if(error.response){
