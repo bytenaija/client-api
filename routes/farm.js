eeeaa954b3c4d643400051e452221fe859de78f1
Everistus Olumese <everistusolumese@gmail.com>
2019-02-10 04:17:36 +0100

    transactions

--- routes/farm.js
+++ routes/farm.js
@@ -7,7 +7,7 @@ const FarmController = require('../controllers/Farms')
 
 router.get('/v1/', FarmController.getAllFarms)
 router.post('/v1/', verifyToken, FarmController.createFarm)
-router.patch('/v1/:id', verifyToken, FarmController.editFarm)
+router.patch('/v1/:id',  FarmController.editFarm)
 router.post('/v1/withdraw', verifyToken, FarmController.withDrawROI)
 router.delete('/v1/:id',  FarmController.deleteFarm)
 
