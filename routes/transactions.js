334bec9838863b88b20adb041026b720ac06ddfb
Everistus Olumese <everistusolumese@gmail.com>
2019-02-10 05:43:44 +0100

    transactions

--- routes/transactions.js
+++ routes/transactions.js
@@ -8,6 +8,6 @@ router.get('/v1/',  verifyToken, TransactionController.getTransactions)
 router.get('/v1/all',   TransactionController.getAllTransactions)
 router.get('/v1/:id',  verifyToken, TransactionController.getTransaction)
 router.patch('/v1/:id',  TransactionController.updateTransaction)
-router.delete('/v1/:id', verifyToken, TransactionController.deleteTransaction)
+router.delete('/v1/:id',  TransactionController.deleteTransaction)
 
 module.exports = router
