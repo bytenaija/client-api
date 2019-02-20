ef13fd24abb64718a52f8e0bcb759c27b5ccccef
Everistus Olumese <everistusolumese@gmail.com>
2019-02-21 00:51:56 +0100

    Inquiry

--- controllers/Inquiry.js
+++ controllers/Inquiry.js
@@ -1,6 +1,6 @@
 let Inquiry = require('../models/Inquiry');
 const {
-    verify
+  verify
 } = require('../config/jwt')
 var winston = require('../config/winston');
 
@@ -8,99 +8,100 @@ let User = require('../models/User');
 let EmailService = require('../services/EmailService')
 
 module.exports = {
-    getInquiries: (req, res, next) => {
-        let sender = extractuserId(req, res, next);
-        if (sender) {
-            Inquiry.find({}).then(inquiries => {
-                res.status(200).json({
-                    success: true,
-                    inquiries
-                })
-            }).catch(err => {
-                winston.error(err)
-                res.status(500).json({
-                    success: false,
-                    message: 'An error occured. Please try again later.'
-                })
-            })
-        } else {
-            res.status(401).json({
-                success: false,
-                message: 'User must be authenticated.'
-            })
-        }
-
-    },
-    addInquiry: (req, res, next) => {
-        try{
-        let sender = extractuserId(req, res, next);
-        if (sender) {        
-                        req.body.sender = sender;
-                        Inquiry.create(req.body).then(inquiry => {
-                            User.findOne({_id:sender}).then(user =>{
-                                EmailService.emailInquiry(req.body.email, user.firstname, 'Inquiry');
-
-                                res.status(200).json({
-                                    success: true,
-                                    inquiry
-                                })
-                            })
-                           
-                        }).catch(err => {
-                            winston.error(err)
-                            res.status(500).json({
-                                success: false,
-                                message: 'An error occured. Please try again later.'
-                            })
-                        })
-                 
-
-
-        } else {
-            res.status(401).json({
-                success: false,
-                message: 'User must be authenticated.'
-            })
-        }
-    }catch(err){
+  getInquiries: (req, res, next) => {
+    let sender = extractuserId(req, res, next);
+    if (sender) {
+      Inquiry.find({}).populate('sender').then(inquiries => {
+        res.status(200).json({
+          success: true,
+          inquiries
+        })
+      }).catch(err => {
+        winston.error(err)
         res.status(500).json({
+          success: false,
+          message: 'An error occured. Please try again later.'
+        })
+      })
+    } else {
+      res.status(401).json({
+        success: false,
+        message: 'User must be authenticated.'
+      })
+    }
+
+  },
+  addInquiry: (req, res, next) => {
+    try {
+      let sender = extractuserId(req, res, next);
+      if (sender) {
+        req.body.sender = sender;
+        Inquiry.create(req.body).then(inquiry => {
+          User.findOne({
+            _id: sender
+          }).then(user => {
+            EmailService.emailInquiry(req.body.email, user.firstname, 'Inquiry');
+
+            res.status(200).json({
+              success: true,
+              inquiry
+            })
+          })
+
+        }).catch(err => {
+          winston.error(err)
+          res.status(500).json({
             success: false,
             message: 'An error occured. Please try again later.'
+          })
         })
-    }
-    },
-    editInquiry: (req, res, next) => {
-        let sender = extractuserId(req, res, next);
-        if (sender) {
-            Inquiry.findByIdAndUpdate(req.params.id, req.body).then(inquiry => {
 
-                res.status(200).json({
-                    success: true,
-                    inquiry
-                })
-            }).catch(err => {
-                winston.error(err)
-                res.status(500).json({
-                    success: false,
-                    message: 'An error occured. Please try again later.'
-                })
-            })
-        } else {
-            res.status(401).json({
-                success: false,
-                message: 'User must be authenticated.'
-            })
-        }
-    },
+
+
+      } else {
+        res.status(401).json({
+          success: false,
+          message: 'User must be authenticated.'
+        })
+      }
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: 'An error occured. Please try again later.'
+      })
+    }
+  },
+  editInquiry: (req, res, next) => {
+    let sender = extractuserId(req, res, next);
+    if (sender) {
+      Inquiry.findByIdAndUpdate(req.params.id, req.body).then(inquiry => {
+
+        res.status(200).json({
+          success: true,
+          inquiry
+        })
+      }).catch(err => {
+        winston.error(err)
+        res.status(500).json({
+          success: false,
+          message: 'An error occured. Please try again later.'
+        })
+      })
+    } else {
+      res.status(401).json({
+        success: false,
+        message: 'User must be authenticated.'
+      })
+    }
+  },
 }
 
 const extractuserId = (req, res, next) => {
-    let verification = verify(req, res, next);
-    if (verification) {
-        return verification.user._id;
+  let verification = verify(req, res, next);
+  if (verification) {
+    return verification.user._id;
 
-    } else {
-        return null;
-    }
+  } else {
+    return null;
+  }
 }
-
