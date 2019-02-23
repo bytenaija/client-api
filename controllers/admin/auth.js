90770799752c8d3106a5d3b7d2afdc88800bf000
Everistus Olumese <everistusolumese@gmail.com>
2019-02-23 08:12:21 +0100

    Admin setup

--- controllers/admin/auth.js
+++ controllers/admin/auth.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const Admin = require('../../models/admin/User');
 const {jwtVerify, jwtSign} = require('../../config/jwt')
 
@@ -10,14 +12,21 @@ module.exports = {
         if(!admin){
           return res.status(404).json({success: false, message: 'Invalid credentials'})
         }else{
-          if(admin.comparePassword(password)){
-            let token = jwtSign(admin);
+          admin.comparePassword(password, (err, isMatch) => {
+                if (isMatch) {
+                  let body = {
+                    _id: admin._id,
+                    email: admin.email
+                  };
+
+            let token = jwtSign(body);
             res.status(200).json({success: true, token, admin})
           }else{
             return res.status(404).json({success: false, message: 'Invalid credentials'})
           }
-        }
-      })
+        })
+      }
+    })
       .catch(err =>{
         console.log(err)
         return res.status(500).json({success: false, message: 'An error occured. Please try again later'})
